8c8f7d30f1c9490fc0cf3651310382312dc240ca
moki-daniel <mokidnl@gmail.com>
2019-02-22 08:35:22 +0300

    [start #164160714] commended on the code for both html and JS files

--- deleteuser.js
+++ deleteuser.js
@@ -1,7 +1,9 @@
 function deluser()
 {
+    //enter name prompt pop-up
     var deleteuser = prompt("enter name of the user to delete");
     if (deleteuser != null){
+        //confirm pop-up
         confirm("Press ok to delete " + deleteuser +" otherwise cancel");
     }   
 }
