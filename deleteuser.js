97571cb3953e75104d3a75eb187d9db53afaebe8
moki-daniel <mokidnl@gmail.com>
2019-02-22 08:32:01 +0300

    [start #164160714] linked delete a user button to the prompt and confirm pop-ups

--- deleteuser.js
+++ deleteuser.js
@@ -2,6 +2,6 @@ function deluser()
 {
     var deleteuser = prompt("enter name of the user to delete");
     if (deleteuser != null){
-        confirm("Press ok to confirm delete of  " + deleteuser + "username");
+        confirm("Press ok to delete " + deleteuser +" otherwise cancel");
     }   
 }
